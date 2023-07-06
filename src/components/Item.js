78a7a64a544145b9d34a1e0e17000b12b79b1978
burcukocal~ <burcukocal97@gmail.comgit config --global user.name burcukocal~git config --global user.email burcukocal97@gmail.com>
2023-07-06 21:06:39 +0300

    yapıldı

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,13 @@
-import React from 'react'
+ import React, { useState } from 'react'
 
 
 function Item({ data }) {
+
   return (
-    <div className='shadow-md bg-white text-center'>
-      <p className='text-2xl p-10'>{data.title}</p>
+    <div  className='shadow-md bg-white text-center'>
+      <p className='text-2xl p-10'>{data.setup}</p>
+      <p className='text-xl p-6 text-lime-800 italic'>{data.punchline}</p>
+      
     </div>
   )
 }
